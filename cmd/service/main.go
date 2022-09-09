e02a8329cbab09e892b4f58712be1d217c192a63
Stefano Scafiti <stefano.scafiti96@gmail.com>
2022-09-09 09:57:04 +0200

    Implement server soft shutdown in order to correctly close storage

--- cmd/service/main.go
+++ cmd/service/main.go
@@ -1,8 +1,15 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"flag"
 	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/ostafen/demo/api"
 	"github.com/ostafen/demo/store"
@@ -15,6 +22,31 @@ const (
 	storagePathDefault = "."
 )
 
+func startServer(server *http.Server) {
+	if err := server.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+		log.Fatal(err)
+	}
+}
+
+func shutdownServer(ctx context.Context, server *http.Server) {
+	ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		log.Fatal(err)
+	} else {
+		log.Println("server successfully stopped")
+	}
+}
+
+func listenSignals() {
+	stopCh := make(chan os.Signal, 1)
+	signal.Notify(stopCh, os.Interrupt, syscall.SIGTERM, syscall.SIGINT)
+
+	<-stopCh
+	close(stopCh)
+}
+
 func main() {
 	storagePath := flag.String("storage", storagePathDefault, "root directory where persistent data will be stored")
 	listenAddr := flag.String("host", addrDefault, "bind address of the server")
@@ -25,6 +57,7 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	defer s.Close()
 
 	controller := api.NewEventController(s)
 
@@ -34,5 +67,11 @@ func main() {
 
 	log.Printf("Starting server on %s with storage path \"%s\"\n", *listenAddr, *storagePath)
 
-	engine.Run(*listenAddr)
+	server := &http.Server{Addr: *listenAddr, Handler: engine}
+	go startServer(server)
+
+	listenSignals()
+
+	log.Println("shutting down server...")
+	shutdownServer(context.Background(), server)
 }
